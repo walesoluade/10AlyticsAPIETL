562704810864298ef6c142cb9565f8f444239f9e
walesoluade <walesoluade@gmail.com>
2023-03-12 09:36:04 -0600

    added version 2

--- test.py
+++ test.py
@@ -5,4 +5,9 @@ def add_nums():
    sum_of_num = var1 + var2
    print(f'The sum of the numbers is {sum_of_num}')
 
-add_nums()
+def multiply_nums():
+   #multiply numbers
+   var1 = 5
+   var2 = 10
+   sum_of_num = var1 * var2
+   print(f'The sum of the numbers is {multiply_nums}')
