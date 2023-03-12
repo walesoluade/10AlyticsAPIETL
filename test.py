22c29bb83806ccce04e47c0f3ac73f8ee8fc76e7
walesoluade <walesoluade@gmail.com>
2023-03-12 09:24:21 -0600

    chnages to the function name

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-def github_learning():
+def add_nums():
    #add numbers
    var1 = 5
    var2 = 10
    sum_of_num = var1 + var2
    print(f'The sum of the numbers is {sum_of_num}')
-   
-github_learning()
+
+add_nums()
