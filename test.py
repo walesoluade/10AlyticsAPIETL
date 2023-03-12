a51267490dedf1a8138ce1695260961573bfe2ae
walesoluade <walesoluade@gmail.com>
2023-03-12 09:18:48 -0600

    a new code has been added

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 def github_learning():
-    print('welcome to github')
-
+   #add numbers
+   var1 = 5
+   var2 = 10
+   sum_of_num = var1 + var2
+   print(f'The sum of the numbers is {sum_of_num}')
+   
 github_learning()
